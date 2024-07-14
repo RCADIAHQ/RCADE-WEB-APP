8fe0c958b1b9b070ab7a5398fdd5d0c989fb8376
Meta Fusion Labs <metafusionlabs@gmail.com>
2024-07-14 12:57:34 +0100

    Update LeaderBoards.js

--- src/Pages/LeaderBoards.js
+++ src/Pages/LeaderBoards.js
@@ -7,7 +7,7 @@ import React from 'react'
 function LeaderBoards() {
     return (
         <div className='leaderboards' style={{ textAlign: 'center', color: 'white' }}>
-          <h1 style={{ marginTop: '400px' }}>LeaderBoards Coming Shoon</h1>
+          <h1 style={{ marginTop: '400px' }}>LeaderBoards Coming Soon</h1>
         </div>
       );
     }
